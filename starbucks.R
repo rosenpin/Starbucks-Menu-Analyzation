42ed4efb9a6af7e604fcfea0938a4d153596faba
rosenpin <mail@tomerrosenfeld.com>
2022-03-09 10:53:52 +0200

    cleaned dataset

--- starbucks.R
+++ starbucks.R
@@ -1,1 +1,3 @@
-dataset = read.csv(file = "https://raw.githubusercontent.com/rosenpin/Starbucks-Menu-Analyzation/master/starbucks_drinkMenu_expanded.csv")
+dataset = read.csv(file = "starbucks_drinkMenu_expanded.csv")
+
+test <- dataset$Calories * dataset$Caffeine..mg.
