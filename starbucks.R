ada9d11aa9a1f01c167f7d450cca9da15b3de1a6
rosenpin <mail@tomerrosenfeld.com>
2022-03-09 11:22:36 +0200

    deleted empty caffeine row

--- starbucks.R
+++ starbucks.R
@@ -1,3 +1,8 @@
 dataset = read.csv(file = "starbucks_drinkMenu_expanded.csv")
 
-test <- dataset$Calories * dataset$Caffeine..mg.
+calories_times_caffeine <- dataset$Calories * dataset$Caffeine..mg.
+#for (i in 10000) {
+  random_sample <- sample(calories_times_caffeine, 100, replace = TRUE)
+  qts <- quantile(random_sample, probs=c(.25,.75), na.rm = TRUE)
+  range <- unname(qts[2]) - unname(qts[1])
+  #}
